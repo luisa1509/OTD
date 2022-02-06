f8c6618c76020a34c803d6b073e9fe04f2aa3767
luisa1509 <luisa_15092000@hotmail.com>
2022-02-05 19:45:07 -0500

    Modo oscuro funcional

--- JS/Instituciones.js
+++ JS/Instituciones.js
@@ -1,16 +1,22 @@
 window.onscroll = function () {
-    window.scrollTo(0,0);
+  window.scrollTo(0,0);
 }
 
+let nav = document.querySelector(".nav_principal");
+let body = document.querySelector("body");
+
 document.getElementById('id-sun').onclick = function(){
-    document.getElementById('body').classList.remove('dark-mode')
-    document.getElementById('id-moon').classList.remove('active')
-    this.classList.add('active')
-  }
-  /*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del div 
-  con id page y se aplicará el estilo active a la luna*/
-  document.getElementById('id-moon').onclick = function(){
-    document.getElementById('body').classList.add('dark-mode')
-    document.getElementById('id-sun').classList.remove('active')
-    this.classList.add('active')
-  }
+  body.classList.remove('dark-mode');
+  nav.classList.remove('dark-mode');
+
+  console.log("darks");
+  
+}
+/*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del div 
+con id page y se aplicará el estilo active a la luna*/
+document.getElementById('id-moon').onclick = function(){
+  body.classList.add('dark-mode')
+  nav.classList.add('dark-mode');
+
+  console.log("darks");
+}
