f82cedbc6fbcb742fbc8092e8126f7d97c431480
luisa1509 <luisa_15092000@hotmail.com>
2022-02-14 22:11:45 -0500

    Cambio imagenes

--- JS/Instituciones.js
+++ JS/Instituciones.js
@@ -3,7 +3,16 @@
 let nav = document.querySelector(".nav_principal");
 let body = document.querySelector("body");
 
+function cambiarLogoDark(){
+  document.getElementById("logo").src="/Imgs/logodark.png";
+}
+
+function cambiarLogoLight(){
+  document.getElementById("logo").src="/Imgs/logolight.png";
+}
+
 document.getElementById('id-sun').onclick = function(){
+  cambiarLogoLight();
   body.classList.remove('dark-mode');
   nav.classList.remove('dark-mode');
 
@@ -13,8 +22,11 @@ document.getElementById('id-sun').onclick = function(){
 /*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del div 
 con id page y se aplicará el estilo active a la luna*/
 document.getElementById('id-moon').onclick = function(){
+  cambiarLogoDark();
   body.classList.add('dark-mode')
   nav.classList.add('dark-mode');
+
+
 
   console.log("darks");
 }
