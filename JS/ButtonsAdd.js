7e28028dc032a70fc28ba54b33992fefb8cb9ae3
luisa1509 <luisa_15092000@hotmail.com>
2022-04-15 20:47:52 -0500

    ButtonAdd funcional

---
+++ JS/ButtonsAdd.js
@@ -0,0 +1,6 @@
+const btngrupos =document.querySelector(".buttonsadd");
+
+btngrupos.addEventListener("click", function(){
+    console.log("hola")
+    window.location.href = 'GruposInvestigacion.html'
+});
