89c46b9f3480de4374c98e3bea5c076826504349
luisa1509 <luisa_15092000@hotmail.com>
2022-04-15 20:22:50 -0500

    Principal completo

--- JS/Principal.js
+++ JS/Principal.js
@@ -1,11 +1,33 @@
 
 let body = document.querySelector("body");
 
+function cambiarImgDark(){
+  document.getElementById("line1").src="./Imgs/LineWhite.png";
+  document.getElementById("line2").src="./Imgs/LineWhite.png";
 
+}
+
+function cambiarImgLight(){
+  document.getElementById("line1").src="./Imgs/LineBlue.png";
+  document.getElementById("line2").src="./Imgs/LineBlue.png";
+
+}
+
+function cambiarLogoDark(){
+  document.getElementById("logoRAD").src="./Imgs/logoRADdark.png";
+  document.getElementById("logoOTD").src="./Imgs/logodark.png";
+}
+
+function cambiarLogoLight(){
+  document.getElementById("logoRAD").src="./Imgs/logoRADlight.png";
+  document.getElementById("logoOTD").src="./Imgs/logolight.png";
+}
 
 document.getElementById('id-sun').onclick = function(){
-
+  cambiarLogoLight();
+  cambiarImgLight()
   body.classList.remove('dark-mode');
+
   
 
   console.log("darks");
@@ -14,12 +36,11 @@ document.getElementById('id-sun').onclick = function(){
 /*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del div 
 con id page y se aplicará el estilo active a la luna*/
 document.getElementById('id-moon').onclick = function(){
-
+  cambiarLogoDark();
+  cambiarImgDark()
   body.classList.add('dark-mode')
+
  
-
-
-
   console.log("darks");
 }
 
