6b1c4a9dd54c6516df7d0c645d4c3899d3787116
luisa1509 <luisa_15092000@hotmail.com>
2022-05-16 06:55:35 -0500

    cambios

--- JS/datosEspecificos.js
+++ JS/datosEspecificos.js
@@ -26,19 +26,23 @@ function renderGroups (elem) {
 const Group = document.createElement('div');
 Group.classList.add('group');
 Group.innerHTML = `
+<div class="first">
 <h1 class="name__group">${elem.name}</h1>
 <a target="_blank" href="${elem.cvlac}">${elem.cvlac}</a>
 <div class ="numbers">
-<img class ="numberImage" src="../Imgs/numero-invest-RAD.png">
+<img class ="numberImage" src="../Imgs/número-investWhite.png">
 <p class="number__invest">${elem.numberOfInvest}</p>
 </div>
-
 <h3 class="areas">${elem.area1}</h3>
 <p class="leader"> <strong> Líder:</strong> ${elem.leader}</p>
 <p class="leader"> <strong> Lugar:</strong> ${elem.town}</p>
 <p class="leader"> <strong> Año de conformación:</strong> ${elem.creationYear}</p>
-<p class="leader"> <strong> Descripción:</strong> ${elem.description}</p>
 <p class="leader"> <strong> Línea de investigación:</strong> ${elem.lineOfResearch}</p>
+</div>
+<div class="description">
+<p class="text"> <strong> Descripción:</strong> ${elem.description}</p>
+</div>
+
 `;
 groupList1.appendChild(Group);
 
